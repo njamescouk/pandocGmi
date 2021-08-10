8678017c8edd7e0f4e5128548db3e5804ce5ef9c
nick james <nickjames@fastmail.com>
2021-08-10 08:36:31 +0100

    typos

--- example/compareGmis.bat
+++ example/compareGmis.bat
@@ -3,6 +3,6 @@ echo comparing pandocGmi.md -^> html -^> gmi
 echo to pandocGmi.md -^> gmi
 echo assuming pandoc on your PATH
 pandoc -s ..\doc\pandocGmi.md > pd.html
-pandoc -f html -t ..\filters\gmi.lua --template ..\template\default.gmi pd.html > pd.html.gmi
-pandoc -t ..\filters\gmi.lua --template ..\template\default.gmi ..\doc\pandocGmi.md > pd.gmi
+pandoc -f html -t ..\filters\gmi.lua --template ..\templates\default.gmi pd.html > pd.html.gmi
+pandoc -t ..\filters\gmi.lua --template ..\templates\default.gmi ..\doc\pandocGmi.md > pd.gmi
 diff pd.gmi pd.html.gmi
